bb55ed9c1620ac94b6d39c397b998e50316515c1
jiayi-pan <jiayipan@umich.edu>
2021-11-17 23:04:05 -0500

    FairAdj Naive Implementation

---
+++ models_to_compare_against/FairAdj/TorchG Implementation/FairAdj/src/model/G_VGAE.py
@@ -0,0 +1,32 @@
+import torch
+from torch import nn
+import torch_geometric.nn as Gnn
+
+class VGAE(nn.Module):
+	def __init__(self, input_feat_dim, hidden_dim1, hidden_dim2):
+		super(VGAE,self).__init__()
+		self.gc1 = Gnn.GCNConv(input_feat_dim,hidden_dim1)
+		self.gc_mean = Gnn.GraphConv(hidden_dim1,hidden_dim2)
+		self.gc_logvar = Gnn.GraphConv(hidden_dim1,hidden_dim2)
+		self.decode = Gnn.InnerProductDecoder()
+	
+	def encode(self, x, adj):
+		hideen1 = self.gc1(x,adj)
+		return self.gc_mean(hideen1,adj), self.gc_logvar(hideen1,adj)
+
+	def reparameterize(self, mu, logvar):
+		if self.training:
+			std = torch.exp(logvar)
+			eps = torch.randn_like(std)
+			return eps.mul(std).add_(mu)
+		else:
+			return mu
+	
+	def forward(self, x, adj):
+		mu, logvar = self.encode(x, adj)
+		z = self.reparameterize(mu, logvar)
+		return self.decode(z), z, mu, logvar
+	
+	def get_parameters(self):
+		return [{"params": self.parameters(), "lr_mult": 1.}]
+	
