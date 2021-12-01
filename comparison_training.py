4d848a28a755861a31c82f7cb916c6558ada23da
Donald Loveland <donaldloveland@Donalds-MacBook-Pro.local>
2021-12-01 16:15:03 -0500

    update comparison training

--- comparison_training.py
+++ comparison_training.py
@@ -17,6 +17,7 @@ def main():
         parser = argparse.ArgumentParser()
 
         parser.add_argument('--model', type=str, default='gcn')
+        parser.add_argument('--dataset', type=str, default='credit')
         args = parser.parse_known_args()[0]
         
         device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
@@ -26,77 +27,66 @@ def main():
         np.random.seed(seed)
         torch.manual_seed(seed)
 
-        dataset = 'german'
-        sens_attr = "Gender"  
-        sens_idx = 0
-        predict_attr = "GoodCustomer"
-        label_number = 100
-        path_german = "./dataset/german"
-        adj, features, labels, idx_train, idx_val, idx_test, sens = load_german(dataset, sens_attr,
-                                                                                predict_attr, path=path_german,
-                                                                                label_number=label_number)
+         #### Load Datasets ####
+        # Load credit_scoring dataset
+        if args.dataset == 'credit':
+                sens_attr = "Age"  # column number after feature process is 1
+                sens_idx = 1
+                predict_attr = 'NoDefaultNextMonth'
+                label_number = 6000
+                path_credit = "./dataset/credit"
+                adj, features, labels, idx_train, idx_val, idx_test, sens = load_credit(args.dataset, sens_attr,
+                                                                                        predict_attr, path=path_credit,
+                                                                                        label_number=label_number
+                                                                                        )
+                norm_features = feature_norm(features)
+                norm_features[:, sens_idx] = features[:, sens_idx]
+                features = norm_features
+
+        # Load german dataset
+        elif args.dataset == 'german':
+                sens_attr = "Gender"  # column number after feature process is 0
+                sens_idx = 0
+                predict_attr = "GoodCustomer"
+                label_number = 100
+                path_german = "./dataset/german"
+                adj, features, labels, idx_train, idx_val, idx_test, sens = load_german(args.dataset, sens_attr,
+                                                                                        predict_attr, path=path_german,
+                                                                                        label_number=label_number,
+                                                                                        )
+                edge_index = convert.from_scipy_sparse_matrix(adj)[0]
+                
+                lr = .01
+                weight_decay = 5e-4
+
+        # Load bail dataset
+        elif args.dataset == 'bail':
+                sens_attr = "WHITE"  # column number after feature process is 0
+                sens_idx = 0
+                predict_attr = "RECID"
+                label_number = 100
+                path_bail = "./dataset/bail"
+                adj, features, labels, idx_train, idx_val, idx_test, sens = load_bail(args.dataset, sens_attr,
+                                                                                        predict_attr, path=path_bail,
+                                                                                        label_number=label_number,
+                                                                                        )
+                norm_features = feature_norm(features)
+                norm_features[:, sens_idx] = features[:, sens_idx]
+                features = norm_features
 
         edge_index = convert.from_scipy_sparse_matrix(adj)[0]
+        num_class = labels.unique().shape[0]-1
         
         lr = .01
         weight_decay = 5e-4
 
         num_class = labels.unique().shape[0]-1
-        if args.model == 'gcn':
-                model = GCN(nfeat=features.shape[1],
-                        nhid=64,
-                        nclass=num_class,
-                        dropout=0.5)
+        if args.model == 'fairadj':
+                pass
 
-        elif args.model == 'sage':
-                model = SAGE(nfeat=features.shape[1],
-                        nhid=64,
-                        nclass=num_class,
-                        dropout=0.5)
-        
-        elif args.model == 'appnp':
-                model = APPNP(nfeat=features.shape[1],
-                        nhid=64,
-                        nclass=num_class,
-                        K=2, alpha=0.1, dropout=0.5)
+        elif args.model == 'fairwalk':
+                pass
 
-        optimizer = optim.Adam(model.parameters(), lr=lr, weight_decay=weight_decay)
-        model = model.to(device)
-
-        t_total = time.time()
-        best_loss = 100
-        epochs = 200
-        best_acc = 0
-        features = features.to(device)
-        edge_index = edge_index.to(device)
-        labels = labels.to(device)
-
-        for epoch in range(epochs):
-                model.train()
-                optimizer.zero_grad()
-                output = model(features, edge_index)
-
-                # Binary Cross-Entropy  
-                preds = (output.squeeze()>0).type_as(labels)
-                loss_train = F.binary_cross_entropy_with_logits(output[idx_train], labels[idx_train].unsqueeze(1).float().to(device))
-
-                auc_roc_train = roc_auc_score(labels.cpu().numpy()[idx_train], output.detach().cpu().numpy()[idx_train])
-                loss_train.backward()
-                optimizer.step()
-
-                # Evaluate validation set performance separately,
-                model.eval()
-                output = model(features, edge_index)
-
-                # Binary Cross-Entropy
-                preds = (output.squeeze()>0).type_as(labels)
-                loss_val = F.binary_cross_entropy_with_logits(output[idx_val], labels[idx_val].unsqueeze(1).float().to(device))
-
-                auc_roc_val = roc_auc_score(labels.cpu().numpy()[idx_val], output.detach().cpu().numpy()[idx_val])
-                f1_val = f1_score(labels[idx_val].cpu().numpy(), preds[idx_val].cpu().numpy())
-
-                if loss_val.item() < best_loss:
-                        best_loss = loss_val.item()
-                        torch.save(model.state_dict(), 'weights/weights_{0}.pt'.format(args.model))
-                
+        #TODO set up training for these methods
+    
 main()
