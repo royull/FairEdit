566e1290173c6ce0848b24255e7a260770ea1945
jiayi-pan <jiayipan@umich.edu>
2021-12-07 01:33:30 -0500

    Done Draft of BruteForce 2

--- training_methods/brute_force.py
+++ training_methods/brute_force.py
@@ -6,11 +6,6 @@ from scipy.sparse import coo_matrix
 import numpy as np
 from torch_geometric.utils import convert
 
-def fair_metric(pred, labels, sens):
-    # TODO: correct it to Counter factual fairness
-    return 0
-
-
 def flipAdj(edge_idx: torch.Tensor,i,j,n):
     # TODO: check if it's efficient enough
 
@@ -54,32 +49,6 @@ class bf_trainer():
         counter_features[:, sense_idx] = 1 - counter_features[:, sense_idx]
         self.counter_features = counter_features
     
-
-    # def Compute_Fair(self):
-    #     # Returns: Counterfactual fainess score, Robusness fairness score
-    #     # Prepare
-    #     idx_test = self.val_idx
-    #     # Mainly from nifty_sota_gnn.py, line 333-341
-    #     self.model.eval()
-    #     output = self.model(self.features.to(self.device), self.edge_index.to(self.device))
-    #     counter_features = self.features.clone
-    #     counter_features[:, self.sens_idx] = 1 - counter_features[:, self.sens_idx]
-    #     counter_output = self.model(counter_features.to(self.devices), self.edge_index.to(self.device))
-    #     noisy_features = self.features.clone() + torch.ones(self.features.shape).normal_(0, 1).to(self.device)
-    #     noisy_output = self.model(noisy_features.to(self.device), self.edge_index.to(self.device))
-    #     # Calc
-    #     # Mainly from nifty_sota_gnn.py, line 366-374
-    #     output_preds = (output.squeeze()>0).type_as(self.labels)
-    #     counter_output_preds = (counter_output.squeeze()>0).type_as(self.labels)
-    #     noisy_output_preds = (noisy_output.squeeze()>0).type_as(self.labels)
-    #     auc_roc_test = roc_auc_score(self.labels.cpu().numpy()[idx_test.cpu()], output.detach().cpu().numpy()[idx_test.cpu()])
-    #     counterfactual_fairness = 1 - (output_preds.eq(counter_output_preds)[idx_test].sum().item()/idx_test.shape[0])
-    #     robustness_score = 1 - (output_preds.eq(noisy_output_preds)[idx_test].sum().item()/idx_test.shape[0])
-
-    #     parity, equality = fair_metric(output_preds[idx_test].cpu().numpy(), self.labels[idx_test].cpu().numpy(), sens[idx_test].numpy())
-    #     f1_s = f1_score(self.labels[idx_test].cpu().numpy(), output_preds[idx_test].cpu().numpy())
-    #     return parity, counterfactual_fairness, robustness_score, auc_roc_test, 
-
 
     def train(self, epochs=200):
 
