8998d6b7805ce3edfc8985d8e41455ae2d08ba68
aareshfb <78177666+aareshfb@users.noreply.github.com>
2021-12-11 14:54:45 -0500

    Add files via upload

---
+++ models_to_compare_against/fairwalk-implementation-master/Fair_Metric.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Dec 10 19:43:38 2021
+
+@author: aares
+"""
+import numpy as np
+
+def fair_metric(pred, labels, sens):
+    '''
+    compute statistical parity (SP) and equal opportunity (EO)
+    '''
+    idx_s0 = sens==0
+    idx_s1 = sens==1
+    idx_s0_y1 = np.bitwise_and(idx_s0, labels==1)
+    idx_s1_y1 = np.bitwise_and(idx_s1, labels==1)
+    parity = abs(sum(pred[idx_s0])/sum(idx_s0)-sum(pred[idx_s1])/sum(idx_s1))
+    equality = abs(sum(pred[idx_s0_y1])/sum(idx_s0_y1)-sum(pred[idx_s1_y1])/sum(idx_s1_y1))
+    return parity.item(), equality.item()
